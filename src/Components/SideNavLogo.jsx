6676d456c7817e9d65632e65466e3dc8894f42bc
Ebube_felix <felixebus042@gmail.com>
2025-07-12 16:20:11 -0700

    finishes the sidenav

--- src/Components/SideNavLogo.jsx
+++ src/Components/SideNavLogo.jsx
@@ -1,6 +1,6 @@
 function SideNavLogo() {
   return (
-    <div className="nav-cont">
+    <div className="navContainer">
       <div className="nav-header-cont">
         <div className="nav-header">
           To-D
@@ -28,72 +28,74 @@ function SideNavLogo() {
           </div>
         </div>
       </div>
-      <div className="side-nav-sub-header">
-        <div className="profile">
-          <div className="profile-img"></div>
-          <div className="profile-name">userimpressionnlesss</div>
-          <div className="edit-btn">
-            <img src="/images/down.png" alt="" />
+      <div className="nav-cont-body">
+        <div className="side-nav-sub-header">
+          <div className="profile">
+            <div className="profile-img"></div>
+            <div className="profile-name">userimpressionnlesss</div>
+            <div className="edit-btn">
+              <img src="/images/down.png" alt="" />
+            </div>
+          </div>
+          <div className="notification">
+            <div className="notification-icon  notification-item">
+              <img src="/images/bell-black.png" alt="" />
+            </div>
+            <div className="side-bar-icon notification-item">
+              <img src="/images/sidebar-black.png" alt="" />
+            </div>
           </div>
         </div>
-        <div className="notification">
-          <div className="notification-icon  notification-item">
-            <img src="/images/bell-black.png" alt="" />
+
+        <div className="navItem search">
+          <img src="/images/search-icon.png" alt="" />
+          <div className="searchText">Search</div>
+        </div>
+        <div className="addtask navItem">
+          <div className="addtaskImg">
+            <img src="/images/plus-white.png" alt="" height={16} />
           </div>
-          <div className="side-bar-icon notification-item">
-            <img src="/images/sidebar-black.png" alt="" />
+
+          <div className="addTaskText">Add task</div>
+        </div>
+        <div className="proSect navItem">
+          <img src="/images/crown.png" alt="" />
+          <div className="proText">PRO Version</div>
+        </div>
+        <div className="category navItem">
+          <div className="category1st">
+            <div className="categoryImg">
+              <img src="/images/category-blue.png" alt="" />
+            </div>
+            <div className="categoryText">Category</div>
+          </div>
+
+          <div className="categoryDropdown">
+            <img src="/images/grey arrow down.png" alt="" />
           </div>
         </div>
-      </div>
+        <div className="nav-hr"></div>
 
-      <div className="navItem search">
-        <img src="/images/search-icon.png" alt="" />
-        <div className="searchText">Search</div>
-      </div>
-      <div className="addtask navItem">
-        <div className="addtaskImg">
-          <img src="/images/plus-white.png" alt="" height={16} />
+        <div className="navItem theme">
+          <img src="/images/theme-blue.png" alt="" />
+          <div className="themeText">Theme</div>
         </div>
-
-        <div className="addTaskText">Add task</div>
-      </div>
-      <div className="proSect navItem">
-        <img src="/images/crown.png" alt="" />
-        <div className="proText">PRO Version</div>
-      </div>
-      <div className="category navItem">
-        <div className="category1st">
-          <div className="categoryImg">
-            <img src="/images/category-blue.png" alt="" />
-          </div>
-          <div className="categoryText">Category</div>
+        <div className="donate navItem">
+          <img src="/images/blue-donate-icon.png" alt="" />
+          <div className="themeText">Donate</div>
         </div>
-
-        <div className="categoryDropdown">
-          <img src="/images/grey arrow down.png" alt="" />
+        <div className="feedback navItem">
+          <img src="/images/feedback-blue.png" alt="" />
+          <div className="feedbackText">Feedback</div>
         </div>
-      </div>
-      <div className="nav-hr"></div>
-
-      <div className="navItem theme">
-        <img src="/images/theme-blue.png" height={40} alt="" />
-        <div className="themeText">Theme</div>
-      </div>
-      <div className="donate navItem">
-        <img src="/images/blue-donate-icon.png" alt="" height={40} />
-        <div className="themeText">Donate</div>
-      </div>
-      <div className="feedback navItem">
-        <img src="/images/feedback-blue.png" alt="" height={40} />
-        <div className="feedbackText">Feedback</div>
-      </div>
-      {/* <div className="settings navItem">
+        {/* <div className="settings navItem">
         <img src="/images/settings-blue.png" alt="" height={40} />
         <div className="settingsText">Settings</div>
       </div> */}
-      <div className="share navItem">
-        <img src="/images/share-blue.png" alt="" height={40} />
-        <div className="settingsText">Share App</div>
+        <div className="share navItem">
+          <img src="/images/share-blue.png" alt="" />
+          <div className="settingsText">Share App</div>
+        </div>
       </div>
     </div>
   );
