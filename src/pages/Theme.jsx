f94199f81f8dcdd9aa643a6b69161ad1e27ec571
Ebube_felix <felixebus042@gmail.com>
2025-07-16 14:52:50 -0700

    hell

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -8,10 +8,18 @@ function Theme() {
         <div className="themeTitle">Theme</div>
       </div>
       <div className="themeCont">
-        <div className="purpleTheme themeItem"></div>
-        <div className="orangeTheme themeItem"></div>
-        <div className="greenTheme themeItem"></div>
-        <div className="blackTheme themeItem"></div>
+        <div className="blackTheme themeItem">
+          <div className="themeText">Black</div>
+        </div>
+        <div className="purpleTheme themeItem">
+          <div className="themeText">lightpink</div>
+        </div>
+        <div className="orangeTheme themeItem">
+          <div className="themeText">Orange </div>
+        </div>
+        <div className="greenTheme themeItem">
+          <div className="themeText">cyan</div>
+        </div>
       </div>
     </>
   );
