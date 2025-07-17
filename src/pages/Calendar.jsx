c42399a8a61ef601d46906111d7817b6b9aea1b2
Ebube_felix <felixebus042@gmail.com>
2025-07-16 22:23:08 -0700

    hello

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -6,26 +6,15 @@ import "react-calendar/dist/Calendar.css";
 function Calendar() {
   return (
     <>
-      {/* <div className="calenderHeader">
-        <div className="calheadRight">
-          <div className="backMonth">
-            <img src="/images/backward arrow.png" alt="" height={40} />
-          </div>
-          <div className="calHeadText">JULY 2025</div>
-
-          <div className="nextMonth">
-            <img src="/images/forward arrow.png" alt="" height={24} />
-          </div>
-        </div>
-        <div className="calheadleft">
-          <img src="/images/three-dots-grey.png" alt="" height={30} />
-        </div>
-      </div> */}
       <div className="calheadleft">
         <img src="/images/three-dots-grey.png" alt="" height={30} />
       </div>
       <div className="calendarContainer">
         <CalendarLib />
+      </div>
+      <div className="calNotaskText">
+        No task for the day. <br />
+        Click "+" to create your tasks.
       </div>
       <AddTask />
       <FootNav />
